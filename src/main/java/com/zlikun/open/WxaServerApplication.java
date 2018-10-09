c923c44bf0faa00bb50f5b52f9270f81c7ea20f4
zlikun <zlikun-dev@hotmail.com>
2018-10-09 15:02:12 +0800

    初始化工程

---
+++ src/main/java/com/zlikun/open/WxaServerApplication.java
@@ -0,0 +1,16 @@
+package com.zlikun.open;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zlikun
+ * @date 2018-10-09 15:00
+ */
+@SpringBootApplication
+public class WxaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WxaServerApplication.class, args);
+    }
+}
